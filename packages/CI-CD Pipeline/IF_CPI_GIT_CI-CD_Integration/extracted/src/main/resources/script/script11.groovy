cf49c77296e92d9f2084c0cc92e897a7f9c27495
Anuradha504 <anuradha.naik@innodhee.com>
2026-07-17 17:20:34 +0530

    Delete packages/CI-CD Pipeline/IF_CPI_GIT_CI-CD_Integration directory

--- packages/CI-CD Pipeline/IF_CPI_GIT_CI-CD_Integration/extracted/src/main/resources/script/script11.groovy
+++
@@ -1,9 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-def Message processData(Message message) {
-
-    def fileName = message.getHeader("CamelFileName")
-    message.setProperty("debugFileName", fileName)
-
-    return message
-}
--- packages/CI-CD Pipeline/IF_CPI_GIT_CI-CD_Integration/extracted/src/main/resources/script/script3.groovy
+++
@@ -1,7 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-def Message processData(Message message) {
-    byte[] zipBytes = message.getBody(byte[].class)
-    message.setProperty("zipBinary", zipBytes)
-    return message
-}
